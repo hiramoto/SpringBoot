5f6b504a59e6f027df484c8663f9c58d94bf8b52
hiramoto <akira.hiramoto@gmail.com>
2020-06-11 17:37:03 +0900

    Initial Commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloController 
+{
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello world!";
+    }
+
+    @RequestMapping("test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+}
